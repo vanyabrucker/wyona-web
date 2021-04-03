d87be7e2e67bab9fef4a510da664f4f6d183e15a
Michael Wechner <michael.wechner@wyona.com>
2021-04-03 12:15:19 +0200

    URL fixed

--- components/main/main.js
+++ components/main/main.js
@@ -8,7 +8,7 @@ export function Main() {
                 <img className={styles.heroimage} src={"/media/hero-gradient.png"} alt={"blue and red background gradient image"}></img>
                 <div className={`padding--top--header bx--grid flex flex-wrap--wrap margin--top--layout-07 margin--bottom--layout-06 ${styles.hero}`}>
                     <h1 className="display-01 margin--top--spacing-06 color--white "><span className={"opacity--light"}>Hello, we're Wyona. We develop </span><span className={`a--internal hover--opacity ${styles.hero__a}`}>
-                            <Link href="http://www.ukatie.com" passHref={true}><a className="color--white">Katie</a></Link>
+                            <Link href="https://ukatie.com" passHref={true}><a className="color--white">Katie</a></Link>
                         </span><span className={"opacity--light"}>, a hybrid intelligence based bot, to support human communication.</span>
                     </h1>
                 </div>
