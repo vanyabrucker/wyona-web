4f1d38abf0b206afca44c9bd4c2d843e4ee814cb
Michael Wechner <michael.wechner@wyona.com>
2021-04-04 14:55:16 +0200

    obsolete text removed

--- pages/investors.js
+++ pages/investors.js
@@ -31,11 +31,8 @@ export default function Investors({ allPostsData }) {
                 <section className="margin--top--layout-06 flex-direction--column flex--align-items--center">
                     <div className="bx--grid flex-direction--column">
                         <h1 className="margin--top--spacing-04 expressive-heading-05">Investor Relations</h1>
-                        <p className="margin--top--spacing-06 text-length--02">Crosscut is the largest seed-stage venture capital firm in Los Angeles with deep operational and investing expertise. With decades of experience building the venture capital ecosystem, we leverage our wide network to grow companies across the US and help make your vision a reality.</p>
-                        <p className="margin--top--spacing-06 text-length--02">If you want to apply for an investment please <span
-                            className="a--internal hover--opacity">
-                        <a href="mailto:contact@wyona.com">contact us</a></span>.</p>
-                        <h2 className={"expressive-heading-04 margin--top--layout-05"}>News</h2>
+                        <p className="margin--top--spacing-06 text-length--02">TODO</p>
+                        <h2 className={"expressive-heading-04 margin--top--layout-05"}>Latest News</h2>
                         <ul className="ul--no-styles flex flex-wrap--wrap flex-direction--column margin--top--spacing-06">
                             {allPostsData.map(({ id, date, title }) => (
                                 <PostItem id={id} date={date} title={title}></PostItem>
