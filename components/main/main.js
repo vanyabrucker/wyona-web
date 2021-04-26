40c6efb79a2c437dad6ece960c6586513a0600c5
bruckervanya <vanyabrucker@gmail.com>
2021-04-26 12:55:01 +0200

    improved hero-gradient image size

--- components/main/main.js
+++ components/main/main.js
@@ -5,7 +5,12 @@ export function Main() {
     return(
         <main>
             <section style={{position: "relative", height: "100vh", width: "100%"}} className="flex--justify-content--center background-color--grey-100">
-                <img className={styles.heroimage} src={"/media/hero-gradient.png"} alt={"blue and red background gradient image"}></img>
+                <picture>
+                    <source src={"/media/hero-gradient.webp"} type="image/webp"/>
+                    <source src={"/media/hero-gradient.jp2"} type="image/jp2"/>
+                    <source src={"/media/hero-gradient.png"} type="image/png"/>
+                    <img className={styles.heroimage} src={"/media/hero-gradient.png"} alt={"blue and red background gradient image"}></img>
+                </picture>
                 <div className={`padding--top--header bx--grid flex flex-wrap--wrap margin--top--layout-07 margin--bottom--layout-06 ${styles.hero}`}>
                     <h1 className="display-01 margin--top--spacing-06 color--white "><span className={"opacity--light"}>Hello, we're Wyona. We develop </span><span className={`a--internal hover--opacity ${styles.hero__a}`}>
                             <Link href="https://ukatie.com" passHref={true}><a className="color--white">Katie</a></Link>
