d8834eef8f0c7755a046d3f86f83dfc2053646b1
Michael Wechner <michael.wechner@wyona.com>
2021-04-27 09:28:07 +0200

    description improved

--- pages/posts/ux-engineer.js
+++ pages/posts/ux-engineer.js
@@ -19,8 +19,8 @@ export default function JobDescription() {
                     <div className="bx--grid flex-direction--column flex--align-items--center">
                         <h1 className="expressive-heading-05 col-7">UX Engineer / Angular</h1>
                         <div className="col-7 margin--top--spacing-07">
-                            <p>Our ambition is to develop a QnA / Knowledge management bot which goes beyond providing intuitive answers to questions.</p>
-                            <p className="margin--top--spacing-06">The Research Engineer role is responsible for building NLP / NLU systems that can perform previously impossible tasks or achieve unprecedented levels of performance. This requires good engineering (for example designing, implementing, and improving a massive-scale distributed machine learning system), writing bug-free machine learning code, and building the science behind the algorithms employed.</p>
+                            <p>Katie is a hybrid intelligence based bot, which means humans are continuously helping the AI to improve. Our ambition is to make this feedback loop as simple and as intuitive as possible.</p>
+                            <p className="margin--top--spacing-06">The UX Engineer role is responsible for building the front end of Katie that people love. We strive to learn and understand our users’ needs, behaviors, and emotions to gather insights that inform product strategy and design. Your creativity, analytical skills, and knowledge of user facing technologies will help to launch Katie to a global audience.</p>
                             <p className="margin--top--spacing-06 text-length--02">
                                 Apply at <span><a className={"a--internal"} href="mailto:contact@wyona.com?subject=Career">contact@wyona.com</a></span>.
                             </p>
