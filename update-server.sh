9a5d72096b5a714e34c62f3fd80707b97adcaf7e
Michael Wechner <michael.wechner@wyona.com>
2021-04-28 00:51:39 +0200

    script improved

--- update-server.sh
+++ update-server.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-echo "Update server ..."
-
+echo "INFO: Generate static export ..."
+echo ""
 npm run export
-find out -name "*.html" -print
+
+echo ""
+echo "INFO: Rename HTML files ..."
+echo ""
+#find out -name "*.html" -print
+array=(`find out -name '*.html'`)
+for file in "${array[@]}"
+do :
+    echo "Remove html suffix from $file"
+    mv $file `echo $file | sed 's/.html$//'`
+done
+
+mv out/index out/index.html
+mv out/404 out/404.html
+
+echo ""
+echo "INFO: Upload export to server ..."
+echo ""
 scp -r out/* wyonawebsite@www.wyona.com:www/.
