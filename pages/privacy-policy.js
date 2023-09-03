d3fda7637df29c0db7dbe809c53958a0dcfa1a37
Michael Wechner <michael.wechner@wyona.com>
2023-09-03 22:02:58 +0200

    privacy policy added

---
+++ pages/privacy-policy.js
@@ -0,0 +1,244 @@
+import Head from 'next/head'
+
+/* Component imports */
+import GlobalHead from "../components/head/globalhead";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+
+export default function PrivacyPolicy() {
+    return (
+        <>
+            <Head>
+                <GlobalHead/>
+                <title>Privacy Policy – Wyona</title>
+                <meta name="description" content="Privacy Policy" />
+            </Head>
+            <Header color={"white"}/>
+            <main className="padding--top--header margin--bottom--layout-06">
+                <section className="margin--top--layout-06 flex-direction--column flex--align-items--center">
+                    <div className="bx--grid flex-direction--column">
+                        {/*<p className="margin--top--spacing-04">Contact</p>*/}
+                        <p className="margin--top--spacing-06 text-length--02">
+                            Privacy Policy of Wyona AG
+                        </p>
+<p className="margin--top--spacing-06">
+  The protection of your data is very important to us. In the following, we would like to explain to you in a transparent and understandable way which, whether and how we process your data.
+</p>
+<p className="margin--top--spacing-06"><strong>I. Name and address of the controller</strong></p>
+<p className="margin--top--spacing-06">
+  The controller within the meaning of the General Data Protection Regulation and other national data protection laws of the member states as well as other data protection regulations is:
+</p>
+<p className="margin--top--spacing-06"/>
+<p>Wyona AG</p>
+<p>Fritz-Fleiner-Weg 9</p>
+<p>8044 Zürich</p>
+<p>Switzerland</p>
+<p>E-mail: <a href="mailto:contact@wyona.com">contact@wyona.com</a></p>
+<p className="margin--top--spacing-06"><strong>II. Contact details of the data protection officer</strong></p>
+<p className="margin--top--spacing-06">Data Protection Officer (Datenschutzbeauftragter)</p>
+<p className="margin--top--spacing-06">Michael Wechner</p>
+<p>Wyona AG</p>
+<p>Fritz-Fleiner-Weg 9</p>
+<p>8044 Zürich</p>
+<p>Switzerland</p>
+<p>E-mail: <a href="mailto:michael.wechner@wyona.com">michael.wechner@wyona.com</a></p>
+<p className="margin--top--spacing-06"><strong>III. General provisions on data processing</strong></p>
+<p className="margin--top--spacing-06"><strong>1. Scope of the processing of personal data</strong></p>
+<p className="margin--top--spacing-06">
+  As a matter of principle, we only collect and use your personal data insofar as this is necessary for the provision of a functional website as well as our contents and services or to initiate and discuss a business relationship, a business contact or to establish, implement and terminate our cooperation and the processing is legally permissible, i.e. purpose-bound and based on a legal basis.
+</p>
+<p className="margin--top--spacing-06"><strong>2. Deletion of data and storage periods</strong></p>
+<p>
+  Your data will be deleted or anonymized as soon as the purpose of processing, in particular storage, ceases to apply. In addition, storage may take place if this has been provided for by the European or national legislator in Union regulations, laws (e.g. GDPR, tax, accounting retention obligations) or other provisions to which the controller is subject.
+</p>
+<p>
+  Data will also be disabled or erased if a retention period prescribed by the aforementioned standards expires, unless there is a need to continue storing the data, e.g. for the execution or performance of a contract.
+</p>
+<p className="margin--top--spacing-06"><strong>IV. Provision of the website and setting of log files</strong></p>
+<p className="margin--top--spacing-06"><strong>1. Description and scope of data processing</strong></p>
+<p>
+  Each time our website is accessed, our system automatically collects data and information from the computer system of the accessing computer.
+</p>
+<p>
+  The following data is collected:
+</p>
+<ul>
+  <li>IP address</li>
+  <li>Approximate location based on IP range</li>
+  <li>Browser type</li>
+  <li>The Wyona pages visited by the users on an anonymized basis</li>
+  <li>Date and duration of the visit on an anonymized basis</li>
+  <li>Operating system</li>
+</ul>
+<p>
+  In the case of a registered user, this data is also stored in the log files of our system. Your IP addresses or other data that enable the data to be assigned to you as a user are not permanently stored in the log files, unless their processing is temporarily necessary, e.g. for the delivery and presentation of our website.
+</p>
+<p className="margin--top--spacing-06"><strong>2. Legal basis for the data processing</strong></p>
+<p>
+  Insofar as your visit to our website serves the purpose of initiating a business contact, establishing, implementing or terminating our cooperation, the legal basis for the temporary processing is Article 6 para. 1 lit. b) GDPR. In other cases, the legal basis is Article 6 para. 1 lit. f GDPR, insofar as our interest in the processing outweighs the interest of the data subject.
+</p>
+<p className="margin--top--spacing-06"><strong>3. Purpose of the data processing</strong></p>
+<p>
+  When visiting our website, the temporary processing, in particular storage, of the IP address by the system is necessary to enable the delivery of the website to the user&#39;s computer. To the latter purpose, the user&#39;s IP address has to be stored for the duration of the session.
+</p>
+<p>
+  These purposes also constitute our legitimate interest in the data processing pursuant to Article 6 para. 1 lit. f) GDPR. Insofar as the visit to our website serves the purpose of initiating, implementing or terminating the business relationship, this purpose at the same time also constitutes the purpose of the processing.
+</p>
+<p className="margin--top--spacing-06"><strong>V. Contact sheet and e-mail correspondence</strong></p>
+<p className="margin--top--spacing-06"><strong>1. Description and scope of data processing</strong></p>
+<p>
+  A contact form is available on our website, which can be used for electronic communications. If a user takes advantage of this option, the data entered in the input mask will be transmitted to us and stored. The Data is:
+</p>
+<ul>
+  <li>First and last name</li>
+  <li>E-mail</li>
+  <li>Company</li>
+  <li>Country</li>
+  <li>Organization type</li>
+  <li>The following data is also stored at the time the message is sent: 
+    <ul>
+      <li>Your IP address</li>
+      <li>Date and time of registration</li>
+    </ul>
+  </li>
+</ul>
+<p>
+  It is possible to contact us via the e-mail address on
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>
+   or, in the case of enquiries about data protection, via 
+        <a href="mailto:michael.wechner@wyona.com">michael.wechner@wyona.com</a>
+   In this case, the personal data transmitted with the e-mail (surname, first name, e-mail address, message content) will be processed.
+</p>
+<p>
+  In this context, the data will not be transferred to third parties. The data is primarily used for processing the conversation or for initiating, implementing or terminating the business relationship.
+</p>
+<p className="margin--top--spacing-06"><strong>2. Legal basis for the data processing</strong></p>
+<p>
+  The legal basis for the processing of data transmitted is the so-called legitimate interest, which exists in the course of sending an e-mail is Article 6 para. 1 lit. f) GDPR.
+</p>
+<p>
+  If the e-mail contact aims at the initiation, implementation and termination of the business relationship, the additional legal basis for the processing is Article 6 para. lit. b) GDPR.
+</p>
+<p className="margin--top--spacing-06"><strong>3. Purpose of the data processing</strong></p>
+<p>
+  The processing of the personal data from the input mask serves mainly to process the contact and communication. When contact is made by e-mail, the necessary legitimate interest in processing the data lies in the processing of the contact.
+</p>
+<p>
+  The other personal data processed during the sending process serve to prevent misuse of the contact form and to ensure the security of our information technology systems.
+</p>
+<p>
+  Insofar as you contact us for the purpose of initiating, implementing or terminating the business relationship, these are also the purposes of the processing.
+</p>
+<p className="margin--top--spacing-06"><strong>VI. Newsletter</strong></p>
+<p className="margin--top--spacing-06"><strong>1. Description and scope of data processing</strong></p>
+<p>
+  You can subscribe to our newsletter, with which we inform you about our current interesting offers, by granting your consent. The advertised goods and services are named in the declaration of consent.
+</p>
+<p>
+  We use the so-called double-opt-in procedure for the register for our newsletter. This means that after you have registered, we will send you an email to the email address you have provided in which we ask you to confirm that you are the owner of the email address provided and that you wish to receive the notifications. If you do not confirm your registration within [24 hours], your information will be disabled and automatically erased after one month. In addition, we store your IP addresses and the times of registration and confirmation. The purpose of the procedure is to be able to prove your registration and, if necessary, to clarify a possible misuse of your personal data.
+</p>
+<p>
+  The only mandatory information for sending the newsletter is your e-mail address. [The provision of further, separately marked data is voluntary and will be used to address you personally.] After your confirmation, we store your e-mail address for the purpose of sending you the newsletter.
+</p>
+<p className="margin--top--spacing-06"><strong>2. Legal basis for the data processing</strong></p>
+<p>
+  The legal basis is Article 6 parag. 1, sentence 1 lit. a) GDPR.
+</p>
+<p className="margin--top--spacing-06"><strong>3. Purpose of the data processing</strong></p>
+<p> 
+  The purpose of the data requested is to send the newsletter to your personal e-mail address in order to fulfill your request for updates about our company or our offers. It may be sent at regular or irregular intervals.
+</p>
+<p className="margin--top--spacing-06"><strong>4. Revocation of consent and objection</strong></p>
+<p className="margin--top--spacing-06"><strong>Revocation:</strong> 
+  You can revoke your consent to receive the newsletter and unsubscribe at any time. You can declare the revocation by clicking on the link provided in every newsletter e-mail, by e-mail to 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>
+        
+  , by contact form or by sending a message to the contact details given in the imprint.
+</p>
+<p className="margin--top--spacing-06"><strong>Objection:</strong>
+  You can also object to the processing for direct marketing purposes at any time. Your personal data collected and processed within the scope of the newsletter will then no longer be processed for these purposes. You can declare your objection to advertising by clicking on the link provided in every newsletter e-mail, by e-mail to 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a> 
+  , by using the contact form or by sending a message to the contact details given in the imprint.
+</p>
+<p className="margin--top--spacing-06"><strong>VII. Rights of the data subject</strong></p>
+<p>
+  If your personal data is processed, you are a data subject within the meaning of the GDPR and you have the following rights vis-à-vis the controller, which you can assert - preferably by e-mail to 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>.
+</p>
+<p className="margin--top--spacing-06"><strong>1. Right to access your data pursuant to Article 15 GDPR</strong></p>
+<p>
+  You can contact us if you would like to know what data we have stored about you. In the event of doubts about your identity, we are entitled to check your identity in accordance with Article 12 para. 6 GDPR.
+</p>
+<p className="margin--top--spacing-06"><strong>2. Right to correct your data pursuant to Article 16 GDPR</strong></p>
+<p>
+  You are entitled to a right of rectification and/or completion vis-à-vis us if the personal data processed are inaccurate or incomplete. The data controller has to implement the correction without delay.
+</p>
+<p className="margin--top--spacing-06"><strong>3. Right to have your data deleted pursuant to Article 17 GDPR</strong></p>
+<p>
+  You have the right to obtain the erasure of your personal data from us without undue delay and the controller has the obligation to erase personal data without undue delay. For example you can contact us if you want us to delete certain data that we have stored about you.
+</p>
+<p>
+  The right to erasure does not apply to the extent that processing is necessary
+</p>
+<ul>
+  <li>for exercising the right of freedom of expression and information;</li>
+  <li>for compliance with a legal obligation which requires processing according to Union or Member State law to which the controller is subject or for the performance of a task carried out in the public interest or in the exercise of official authority vested in the controller;</li>
+  <li>for reasons of public interest in the field of public health pursuant to Article 9 para. 2 lit. h and i as well as Article 9 para. 3 GDPR;</li>
+  <li>for archiving purposes in the public interest, for scientific or historical research purposes or for statistical purposes in accordance with Article Article 89 para. 1 GDPR, insofar as the right referred to in section a) is likely to render impossible or seriously prejudice the achievement of the purposes of such processing, or</li>
+  <li>for the establishment, exercise or defence of legal claims.</li>
+</ul>
+<p className="margin--top--spacing-06"><strong>4. Right to limit data collection pursuant to Article 18 GDPR</strong></p>
+<p>
+  You may request the restriction of the processing of personal data. For example you can contact us if you do not want us to delete your e-mail address, but only to send absolutely necessary e-mails.
+</p>
+<p>
+  Where the processing of personal data has been restricted, those data may be processed, with the exception of their storage, only with your consent or for the establishment, exercise or defence of legal claims or for the protection of the rights of another natural or legal person or for reasons of important public interest of the Union or of a Member State.
+</p>
+<p className="margin--top--spacing-06"><strong>5. Right to data portability pursuant to Article 20 GDPR</strong></p>
+<p>
+  You have the right to receive the personal data concerning you that you have provided to us in a structured, commonly used and machine-readable format. You also have the right to transmit this data to another controller without hindrance from the controller to whom the personal data has been provided, provided that the processing is based on consent pursuant to Article 6 para.1 lit. a GDPR or Article 9 para. 2 lit. a GDPR or a contract pursuant to Article 6 para. 1 lit. b GDPR and para. 2 the processing is carried out by automated means.
+</p>
+<p>
+  In exercising this right you have the right to have the personal data transmitted directly from one controller to another, where technically feasible. The freedoms and rights of other persons must not be affected by this.
+</p>
+<p className="margin--top--spacing-06"><strong>6. Right to object how your data is handled pursuant to Article 21 GDPR</strong></p>
+<p>
+  You have the right to object at any time, on grounds relating to your particular situation, to the processing of personal data concerning you which is carried out on the basis of Article 6 para. 1 lit. e or f GDPR, preferably by email to 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>.
+</p>
+<p>
+  The controller will no longer process the personal data relating to you unless it can demonstrate compelling legitimate grounds for the processing which override your interests, rights and freedoms, or the processing is for the establishment, exercise or defence of legal claims.
+</p>
+<p className="margin--top--spacing-06"><strong>7. Right to withdraw your consent pursuant to Article 7 para. 3 GDPR</strong></p>
+<p>
+  In accordance with Article 7 para. 3 GDPR you have the option to revoke your consent to data processing at any time, preferably by e-mail to 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>. The revocation of consent does not affect the lawfulness of processing based on consent before its revocation.
+</p>
+<p className="margin--top--spacing-06"><strong>8. Right to lodge a complaint with a supervisory authority pursuant to Article 77 GDPR</strong></p>
+<p>
+  Without prejudice to any other administrative or judicial remedy, you have the right to lodge a complaint with a supervisory authority, in particular in the Member State of your habitual residence, place of work or place of the alleged infringement if the you consider that the processing of personal data relating to you violates the GDPR.
+</p>
+<p>
+  The supervisory authority with which the complaint has been lodged will inform the complainant on the progress and the outcome of the complaint including the possibility of a judicial remedy pursuant to Article 78 GDPR.
+</p>
+<p>
+  The supervisory authority in Bern, Switzerland, is the State Commissioner for Data Protection and Freedom of Information. It is possible to lodge a complaint from there online.
+</p>
+<p>
+  You have any questions or suggestions?
+</p>
+<p>
+  Please contact us on 
+        <a href="mailto:contact@wyona.com">contact@wyona.com</a>.
+</p>
+<p>
+  Thanks!
+</p>
+<p><em>Date of the privacy policy: 1 September 2023</em></p>
+                    </div>
+                </section>
+            </main>
+            <Footer/>
+        </>
+    )
+}
